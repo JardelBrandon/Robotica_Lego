1f475d5f919ae854c71604a672c1856fd1e1b823
Jardel <jardelbrandon@gmail.com>
2017-04-07 10:57:26 -0300

    Programas robocup_pegar_preto e robocup_pegar_branco, modificados com uma função com funções adicionadas e variáveis e constantes definidas

--- robocup_pegar_branco.py
+++ robocup_pegar_branco.py
@@ -4,6 +4,17 @@ import ev3dev.ev3 as ev3
 from ev3dev.ev3 import *
 from time   import sleep
 
+LIMIAR_DE_COR = 5
+TEMPO_IR = 100
+TEMPO_CURVA = 1000
+TEMPO_FECHAR_GARRA = 700
+TEMPO_ABRIR_GARRA = 1400
+VELOCIDADE_IR = -500
+VELOCIDADE_VOLTAR = 500
+INTENSIDADE_DA_CURVA = 1500
+VELOCIDADE_ABRIR_GARRA = -500
+VELOCIDADE_FECHAR_GARRA = 500
+
 m_esquerdo = ev3.LargeMotor('outA')
 m_direito = ev3.LargeMotor('outB')
 m_garra = ev3.Motor('outC')
@@ -11,44 +22,50 @@ cl = ev3.ColorSensor('in1')
 cl.mode='COL-AMBIENT'
 tempo_voltar = 0
 
-while cl.value() > 4:
-    m_esquerdo.run_timed(time_sp=100, speed_sp=-600, stop_action='brake')
-    m_direito.run_timed(time_sp=100, speed_sp=-600, stop_action='brake')
+def acelerar(tempo, direcao):
+    m_esquerdo.run_timed(time_sp=tempo, speed_sp=direcao , stop_action='brake')
+    m_direito.run_timed(time_sp=tempo, speed_sp=direcao , stop_action='brake')
     m_esquerdo.wait_while('running')
     m_direito.wait_while('running')
-    tempo_voltar += 100
 
-m_garra.run_timed(time_sp=700, speed_sp=500, stop_action='brake')
-m_garra.wait_while('running')
-sleep(3)
+def curva_esquerda(tempo, direcao, curva):
+    m_esquerdo.run_timed(time_sp=(tempo + curva), speed_sp=direcao, stop_action='brake')
+    m_direito.run_timed(time_sp=tempo, speed_sp=direcao , stop_action='brake')
+    m_esquerdo.wait_while('running')
+    m_direito.wait_while('running')
+
+def curva_direita(tempo, direcao, curva):
+    m_esquerdo.run_timed(time_sp=(tempo + curva), speed_sp=direcao , stop_action='brake')
+    m_direito.run_timed(time_sp=tempo, speed_sp=direcao , stop_action='brake')
+    m_esquerdo.wait_while('running')
+    m_direito.wait_while('running')
+
+def garra(tempo, direcao):
+    m_garra.run_timed(time_sp=tempo, speed_sp=direcao, stop_action='brake')
+    m_garra.wait_while('running')
+
+while cl.value() > LIMIAR_DE_COR :
+    acelerar(TEMPO_IR, VELOCIDADE_IR)
+    tempo_voltar += TEMPO_IR
+
+
+garra(TEMPO_FECHAR_GARRA, VELOCIDADE_FECHAR_GARRA)
+sleep(1)
 cl.mode='COL-COLOR'
-sleep(3)
+sleep(1)
 
 if cl.value() == 6:
     ev3.Sound.beep()
-    m_garra.run_timed(time_sp=800, speed_sp=500, stop_action='brake')
-    m_garra.wait_while('running')
-    m_esquerdo.run_timed(time_sp=tempo_voltar, speed_sp=500, stop_action='brake')
-    m_direito.run_timed(time_sp=tempo_voltar, speed_sp=500,  stop_action='brake')
-    m_esquerdo.wait_while('running')
-    m_direito.wait_while('running')
-    m_garra.run_timed(time_sp=1500, speed_sp=-500, stop_action='brake')
-    m_garra.wait_while('running')
+    garra(TEMPO_FECHAR_GARRA, VELOCIDADE_FECHAR_GARRA)
+    acelerar(tempo_voltar, VELOCIDADE_VOLTAR)
+    garra(TEMPO_ABRIR_GARRA, VELOCIDADE_ABRIR_GARRA)
+
 
 elif cl.value() == 0 or cl.value() == 1:
-    ev3.Sound.speak('Welcome to the E V 3 dev project!').wait()
-    m_garra.run_timed(time_sp=600, speed_sp=500, stop_action='brake')
-    m_garra.wait_while('running')
-    m_esquerdo.run_timed(time_sp=3000, speed_sp=-500, stop_action='brake')
-    m_direito.run_timed(time_sp=500, speed_sp=-500, stop_action='brake')
-    m_esquerdo.wait_while('running')
-    m_direito.wait_while('running')
-    m_garra.run_timed(time_sp=1500, speed_sp=-500, stop_action='brake')
-    m_garra.wait_while('running')
-    m_esquerdo.run_timed(time_sp=3000, speed_sp=500, stop_action='brake')
-    m_direito.run_timed(time_sp=500, speed_sp=500, stop_action='brake')
-    m_esquerdo.wait_while('running')
-    m_direito.wait_while('running')
-    m_esquerdo.run_timed(time_sp=tempo_voltar, speed_sp=500, stop_action='brake')
-    m_direito.run_timed(time_sp=tempo_voltar, speed_sp=500, stop_action='brake')
+    ev3.Sound.speak('Welcome to the E V 3 dev project! EEEEEEEEERRRRROOOOOOUUUUUUUUUUU')
+    garra(TEMPO_FECHAR_GARRA, VELOCIDADE_FECHAR_GARRA)
+    curva_direita(TEMPO_CURVA, VELOCIDADE_IR, INTENSIDADE_DA_CURVA)
+    garra(TEMPO_ABRIR_GARRA, VELOCIDADE_ABRIR_GARRA)
+    curva_direita(TEMPO_CURVA, VELOCIDADE_VOLTAR, INTENSIDADE_DA_CURVA)
+    acelerar(tempo_voltar, VELOCIDADE_VOLTAR)
 
--- robocup_pegar_preto.py
+++ robocup_pegar_preto.py
@@ -4,17 +4,16 @@ import ev3dev.ev3 as ev3
 from ev3dev.ev3 import *
 from time   import sleep
 
-LIMIAR_DE_COR = 10
+LIMIAR_DE_COR = 5
 TEMPO_IR = 100
 TEMPO_CURVA = 1000
 TEMPO_FECHAR_GARRA = 700
 TEMPO_ABRIR_GARRA = 1400
 VELOCIDADE_IR = -500
 VELOCIDADE_VOLTAR = 500
-INTENSIDADE_DA_CURVA = VELOCIDADE_IR / 2
+INTENSIDADE_DA_CURVA = 1500
 VELOCIDADE_ABRIR_GARRA = -500
 VELOCIDADE_FECHAR_GARRA = 500
-
 
 m_esquerdo = ev3.LargeMotor('outA')
 m_direito = ev3.LargeMotor('outB')
@@ -29,15 +28,15 @@ def acelerar(tempo, direcao):
     m_esquerdo.wait_while('running')
     m_direito.wait_while('running')
 
-def curva_direita(tempo, direcao, curva):
-    m_esquerdo.run_timed(time_sp=tempo, speed_sp=(direcao-curva) , stop_action='brake')
+def curva_esquerda(tempo, direcao, curva):
+    m_esquerdo.run_timed(time_sp=(tempo + curva), speed_sp=direcao, stop_action='brake')
     m_direito.run_timed(time_sp=tempo, speed_sp=direcao , stop_action='brake')
     m_esquerdo.wait_while('running')
     m_direito.wait_while('running')
 
-def curva_esquerda(tempo, direcao, curva):
-    m_esquerdo.run_timed(time_sp=tempo, speed_sp=(direcao) , stop_action='brake')
-    m_direito.run_timed(time_sp=tempo, speed_sp=(direcao-curva) , stop_action='brake')
+def curva_direita(tempo, direcao, curva):
+    m_esquerdo.run_timed(time_sp=(tempo + curva), speed_sp=direcao , stop_action='brake')
+    m_direito.run_timed(time_sp=tempo, speed_sp=direcao , stop_action='brake')
     m_esquerdo.wait_while('running')
     m_direito.wait_while('running')
 
@@ -61,11 +60,12 @@ if cl.value() == 0 or cl.value() == 1:
     acelerar(tempo_voltar, VELOCIDADE_VOLTAR)
     garra(TEMPO_ABRIR_GARRA, VELOCIDADE_ABRIR_GARRA)
 
+
 elif cl.value() == 6:
     ev3.Sound.speak('Welcome to the E V 3 dev project! EEEEEEEEERRRRROOOOOOUUUUUUUUUUU')
     garra(TEMPO_FECHAR_GARRA, VELOCIDADE_FECHAR_GARRA)
     curva_direita(TEMPO_CURVA, VELOCIDADE_IR, INTENSIDADE_DA_CURVA)
     garra(TEMPO_ABRIR_GARRA, VELOCIDADE_ABRIR_GARRA)
-    curva_direita(-TEMPO_CURVA, -VELOCIDADE_IR, -INTENSIDADE_DA_CURVA)
+    curva_direita(TEMPO_CURVA, VELOCIDADE_VOLTAR, INTENSIDADE_DA_CURVA)
     acelerar(tempo_voltar, VELOCIDADE_VOLTAR)
 
